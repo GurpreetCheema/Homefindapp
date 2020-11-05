1b29d38693b096e48c0eb585480afb943ae1dc7c
Gurpreet Cheema <34461294+GurpreetCheema@users.noreply.github.com>
2020-11-05 15:09:46 -0500

    styling is still needed

--- src/app/places/discover/discover.page.ts
+++ src/app/places/discover/discover.page.ts
@@ -4,6 +4,9 @@ import { MenuController } from '@ionic/angular';
 import { PlacesService } from '../places.service';
 import { Place } from '../place.model';
 
+import { SegmentChangeEventDetail } from '@ionic/core';
+
+
 @Component({
   selector: 'app-discover',
   templateUrl: './discover.page.html',
@@ -11,17 +14,22 @@ import { Place } from '../place.model';
 })
 export class DiscoverPage implements OnInit {
   loadedPlaces: Place[];
+  listedLoadedPlaces: Place[];
 
   constructor(
     private placesService: PlacesService,
     private menuCtrl: MenuController
-  ) { }
+  ) {}
 
   ngOnInit() {
     this.loadedPlaces = this.placesService.places;
+    this.listedLoadedPlaces = this.loadedPlaces.slice(1);
   }
 
   onOpenMenu() {
     this.menuCtrl.toggle();
   }
+
+  onFilterUpdate(event: CustomEvent<SegmentChangeEventDetail>) {
+    console.log(event.detail);  }
 }
