45018f694d536c9e1923098cc7b671f095d1aea3
Gurpreet Cheema <34461294+GurpreetCheema@users.noreply.github.com>
2020-11-06 20:39:46 -0500

    Edit page is up now:

--- src/app/auth/auth.page.ts
+++ src/app/auth/auth.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { AuthService } from './auth.service';
 import { Router } from '@angular/router';
 import { LoadingController } from '@ionic/angular';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-auth',
@@ -10,6 +11,7 @@ import { LoadingController } from '@ionic/angular';
 })
 export class AuthPage implements OnInit {
   isLoading = false;
+  isLogin = true;
 
   constructor(
     private authService: AuthService, 
@@ -34,6 +36,25 @@ export class AuthPage implements OnInit {
     });
    
     }
+    onSwitchAuthMode(){
+      this.isLogin = !this.isLogin;
+    }
+    onSubmit(form: NgForm) {
+    if (!form.valid)  {
+      return;
+    }
+       const email = form.value.email;
+       const password = form.value.password;
+       console.log(email, password);
+
+       if (this.isLogin)
+{
+      //send a rew to login server
+}  else {
+  //send a req to signup server
+}
+ }
+
 
   }
 
--- src/app/places/offers/new-offer/new-offer.module.ts
+++ src/app/places/offers/new-offer/new-offer.module.ts
@@ -1,17 +1,19 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { FormsModule } from '@angular/forms';
+// import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
 
 import { NewOfferPageRoutingModule } from './new-offer-routing.module';
 
 import { NewOfferPage } from './new-offer.page';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   imports: [
     CommonModule,
-    FormsModule,
+    // FormsModule,
+    ReactiveFormsModule,
     IonicModule,
     NewOfferPageRoutingModule
   ],
