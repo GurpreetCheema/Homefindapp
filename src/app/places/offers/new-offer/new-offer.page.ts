879e57baa49f85c1e15db8f0b23ba0ab64f2da57
Gurpreet Cheema <34461294+GurpreetCheema@users.noreply.github.com>
2020-11-11 17:11:07 -0500

    Booking page is done

--- src/app/places/offers/new-offer/new-offer.page.ts
+++ src/app/places/offers/new-offer/new-offer.page.ts
@@ -1,16 +1,23 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { LoadingController } from '@ionic/angular';
+
+import { PlacesService } from '../../places.service';
 
 @Component({
   selector: 'app-new-offer',
   templateUrl: './new-offer.page.html',
-  styleUrls: ['./new-offer.page.scss'],
+  styleUrls: ['./new-offer.page.scss']
 })
 export class NewOfferPage implements OnInit {
   form: FormGroup;
-  
 
-  constructor() { }
+  constructor(
+    private placesService: PlacesService,
+    private router: Router,
+    private loadingCtrl: LoadingController
+  ) {}
 
   ngOnInit() {
     this.form = new FormGroup({
@@ -18,22 +25,22 @@ export class NewOfferPage implements OnInit {
         updateOn: 'blur',
         validators: [Validators.required]
       }),
-        description: new FormControl(null, {
+      description: new FormControl(null, {
         updateOn: 'blur',
         validators: [Validators.required, Validators.maxLength(180)]
-      }),      
-        price: new FormControl(null, {
+      }),
+      price: new FormControl(null, {
         updateOn: 'blur',
         validators: [Validators.required, Validators.min(1)]
       }),
-       dateFrom: new FormControl(null, {
-        updateOn: 'blur',
-        validators: [Validators.required]
-      }),     
-        dateTo: new FormControl(null, {
+      dateFrom: new FormControl(null, {
         updateOn: 'blur',
         validators: [Validators.required]
       }),
+      dateTo: new FormControl(null, {
+        updateOn: 'blur',
+        validators: [Validators.required]
+      })
     });
   }
 
@@ -41,8 +48,25 @@ export class NewOfferPage implements OnInit {
     if (!this.form.valid) {
       return;
     }
-    console.log(this.form);
-
+    this.loadingCtrl
+      .create({
+        message: 'Creating place...'
+      })
+      .then(loadingEl => {
+        loadingEl.present();
+        this.placesService
+          .addPlace(
+            this.form.value.title,
+            this.form.value.description,
+            +this.form.value.price,
+            new Date(this.form.value.dateFrom),
+            new Date(this.form.value.dateTo)
+          )
+          .subscribe(() => {
+            loadingEl.dismiss();
+            this.form.reset();
+            this.router.navigate(['/places/tabs/offers']);
+          });
+      });
   }
-
 }
--- src/app/places/place.model.ts
+++ src/app/places/place.model.ts
@@ -5,5 +5,9 @@ export class Place {
         public description: string,
         public imageUrl: string,
         public price: number,
+        public availableFrom: Date,
+        public availableTo: Date, 
+        public userId: string
+
     ) {}
 }
